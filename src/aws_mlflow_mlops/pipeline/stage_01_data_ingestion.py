1c88a50c8f60f0fdf75c4e20afac1998f7d77567
Vince Lam <57117625+vince-lam@users.noreply.github.com>
2023-11-13 23:52:33 +0800

    doc(stage_01): Add documentation to data ingestion

--- src/aws_mlflow_mlops/pipeline/stage_01_data_ingestion.py
+++ src/aws_mlflow_mlops/pipeline/stage_01_data_ingestion.py
@@ -7,10 +7,29 @@ STAGE_NAME = "Data Ingestion Stage"
 
 
 class DataIngestionTrainingPipeline:
+    """
+    The DataIngestionTrainingPipeline class is responsible for managing the data ingestion process in the training pipeline.
+
+    This class uses the ConfigurationManager to get the data ingestion configuration, creates a DataIngestion instance with this configuration,
+    and then downloads and extracts the data file.
+    """
+
     def __init__(self):
+        """
+        The constructor for DataIngestionTrainingPipeline class.
+        """
         pass
 
-    def main(self):
+    def main(self) -> None:
+        """
+        The main method to run the data ingestion process in the training pipeline.
+
+        This method gets the data ingestion configuration from the ConfigurationManager, creates a DataIngestion instance with this configuration,
+        downloads the data file, and extracts the data file.
+
+        Returns:
+            None
+        """
         config = ConfigurationManager()
         data_ingestion_config = config.get_data_ingestion_config()
         data_ingestion = DataIngestion(config=data_ingestion_config)
@@ -19,6 +38,9 @@ class DataIngestionTrainingPipeline:
 
 
 if __name__ == "__main__":
+    # This script runs the data ingestion stage of the training pipeline.
+    # It creates an instance of the DataIngestionTrainingPipeline class and calls its main method.
+    # If any exceptions are raised during the execution of the pipeline, they are logged and re-raised.
     try:
         logger.info(f"Running {STAGE_NAME}...")
         data_ingestion_pipeline = DataIngestionTrainingPipeline()
