94e0ccfbb108aa667a07ee35c415d806222c7e55
Vince Lam <57117625+vince-lam@users.noreply.github.com>
2023-11-25 08:45:35 +0800

    feat(config): Fix mypy attribute errors by switching from box variables to dict key strings and add some of model_trainer steps

--- src/aws_mlflow_mlops/config/configuration.py
+++ src/aws_mlflow_mlops/config/configuration.py
@@ -1,8 +1,16 @@
-from src.aws_mlflow_mlops.constants import *
+from pathlib import Path
+from typing import Dict
+
+from src.aws_mlflow_mlops.constants import (
+    CONFIG_FILE_PATH,
+    PARAMS_FILE_PATH,
+    SCHEMA_FILE_PATH,
+)
 from src.aws_mlflow_mlops.entity.config_entity import (
     DataIngestionConfig,
     DataTransformationConfig,
     DataValidationConfig,
+    ModelTrainerConfig,
 )
 from src.aws_mlflow_mlops.utils.common import create_directories, read_yaml
 
@@ -20,9 +28,9 @@ class ConfigurationManager:
 
     def __init__(
         self,
-        config_filepath=CONFIG_FILE_PATH,
-        params_filepath=PARAMS_FILE_PATH,
-        schema_filepath=SCHEMA_FILE_PATH,
+        config_filepath: Path = CONFIG_FILE_PATH,
+        params_filepath: Path = PARAMS_FILE_PATH,
+        schema_filepath: Path = SCHEMA_FILE_PATH,
     ):
         """
         The constructor for ConfigurationManager class.
@@ -32,11 +40,11 @@ class ConfigurationManager:
             params_filepath (str): The path to the parameters YAML file.
             schema_filepath (str): The path to the schema YAML file.
         """
-        self.config = read_yaml(config_filepath)
-        self.params = read_yaml(params_filepath)
-        self.schema = read_yaml(schema_filepath)
+        self.config: Dict = read_yaml(config_filepath)
+        self.params: Dict = read_yaml(params_filepath)
+        self.schema: Dict = read_yaml(schema_filepath)
 
-        create_directories([self.config.artifacts_root])
+        create_directories([self.config["artifacts_root"]])
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         """
@@ -45,15 +53,15 @@ class ConfigurationManager:
         Returns:
             DataIngestionConfig: An object containing the configuration for the data ingestion process.
         """
-        config = self.config.data_ingestion
+        config = self.config["data_ingestion"]
 
-        create_directories([config.root_dir])
+        create_directories([config["root_dir"]])
 
         data_ingestion_config = DataIngestionConfig(
-            root_dir=config.root_dir,
-            source_URL=config.source_URL,
-            local_data_file=config.local_data_file,
-            unzip_dir=config.unzip_dir,
+            root_dir=config["root_dir"],
+            source_URL=config["source_URL"],
+            local_data_file=config["local_data_file"],
+            unzip_dir=config["unzip_dir"],
         )
 
         return data_ingestion_config
@@ -68,15 +76,15 @@ class ConfigurationManager:
         Returns:
             DataValidationConfig: An object containing the configuration for the data validation process.
         """
-        config = self.config.data_validation
-        schema = self.schema.COLUMNS
+        config = self.config["data_validation"]
+        schema = self.schema["COLUMNS"]
 
-        create_directories([config.root_dir])
+        create_directories([config["root_dir"]])
 
         data_validation_config = DataValidationConfig(
-            root_dir=config.root_dir,
-            STATUS_FILE=config.STATUS_FILE,
-            unzip_data_dir=config.unzip_data_dir,
+            root_dir=config["root_dir"],
+            STATUS_FILE=config["STATUS_FILE"],
+            unzip_data_dir=config["unzip_data_dir"],
             all_schema=schema,
         )
 
@@ -93,13 +101,46 @@ class ConfigurationManager:
         Returns:
             DataTransformationConfig: An instance of DataTransformationConfig with the root directory and data path from the configuration.
         """
-        config = self.config.data_transformation
+        config = self.config["data_transformation"]
 
-        create_directories([config.root_dir])
+        create_directories([config["root_dir"]])
 
         data_transformation_config = DataTransformationConfig(
-            root_dir=config.root_dir,
-            data_path=config.data_path,
+            root_dir=config["root_dir"],
+            data_path=config["data_path"],
         )
 
         return data_transformation_config
+
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        """
+        The method to get the model trainer configuration.
+
+        This method retrieves the model trainer configuration from the main configuration object,
+        the ElasticNet parameters, and the target column schema.
+        It creates the directories for the root directory in the configuration if they do not exist,
+        creates a ModelTrainerConfig instance with the root directory, train data path, test data path, model name,
+        alpha, l1_ratio, and target column name from the configuration, parameters, and schema,
+        and returns this instance.
+
+        Returns:
+            ModelTrainerConfig: An instance of ModelTrainerConfig with the root directory, train data path, test data path,
+            model name, alpha, l1_ratio, and target column name from the configuration, parameters, and schema.
+        """
+        config = self.config["model_trainer"]
+        params = self.params["ElasticNet"]
+        schema = self.schema["TARGET_COLUMN"]
+
+        create_directories([config["root_dir"]])
+
+        model_trainer_config = ModelTrainerConfig(
+            root_dir=config["root_dir"],
+            train_data_path=config["train_data_path"],
+            test_data_path=config["test_data_path"],
+            model_name=config["model_name"],
+            alpha=params["alpha"],
+            l1_ratio=params["l1_ratio"],
+            target_column=schema["name"],
+        )
+
+        return model_trainer_config
